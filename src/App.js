d83b877cbcdd6b54e12fc242fda3d04021ddc608
Michael Wong <michael.w228@gmail.com>
2021-03-29 19:11:57 -0400

    added components and created navbar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import { useState } from "react"
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, } from "react-router-dom";
+import Navbar from "./components/Navbar"
+// import Contact from "./components/Contact"
+// import About from "./components/About"
+// import Portfolio from "./components/Portfolio"
 // import Footer from "./components/Footer"
-// import PageAbout from "./components/PageAbout"
-// import PageDiscover from "./components/PageDiscover"
-// import PageSearch from "./components/PageSearch"
-
-// function Header(){
-//   return (
-//     <div>Pupster
-//       <NavLink to="/about">About</NavLink> | 
-//       <NavLink to="/discover">Discover</NavLink> | 
-//       <NavLink to="/search">Search</NavLink>
-//     </div>
-//   )
-// }
 
 function App() { 
 
-
-  // html
   return (
     <>
       <Router> 
-        Hello 
-        {/* <Header />
-        <Route exact path={["/","/about"]} component={PageAbout} />
-        <Route exact path="/discover" component={PageDiscover} />
-        <Route exact path="/search" component={PageSearch} /> */}
+        <Navbar />
+        {/* <Route exact path={["/","/about"]} component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />  */}
       </Router>
       {/* <Footer /> */}
     </>
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom"
+
+function Navbar() {
+
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <NavLink className="navbar-brand" to="/">Michael Wong</NavLink>
+            <div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <NavLink to="/" className="nav-link" activeClassName="active">About</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/portfolio" className="nav-link" activeClassName="active">Portfolio</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/contact" className="nav-link" activeClassName="active">Contact</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar
