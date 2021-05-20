19b305707729b89b22aec588c8ffba6ee9351853
Michael Wong <michael.w228@gmail.com>
2021-05-20 14:20:39 -0400

    updated email

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,7 +14,7 @@ function About() {
                         <div class="card-body">
                             <h1 class="card-title">About Me</h1>
                             <p class="card-text">Full Stack Developer with an Economics and Accounting background that can leverage my expertise in Data Analytics to help create robust web applications. Applied UX and Agile development in recent E-Commerce project to develop a single-page MERN application to showcase products, track sales and inventory by pulling information from database results. Adept at collaborating with teams from different departments and strong communication skills when conveying information to others. Bring experience obtained through a cross-functional organization to streamline data analysis, reporting, and auditing.</p>
-                            <p>Contact me via E-mail: michael.w228@gmail.com</p>
+                            <p>Contact me via E-mail: <a href="michael.w228@gmail.com">Michael.w228@gmail.com</a></p>
                             <p>Visit My: <a href="https://www.linkedin.com/in/mwong228/" >LinkedIn Page</a></p>
                             <p>Visit My: <a href="https://github.com/Mwong228" > GitHub Page</a></p>
                             <p>Copy of My: <a href="https://drive.google.com/file/d/1-I1Z07UeOkC4SaaAYdaHBpdioxdZfrPe/view">Resume</a></p>
